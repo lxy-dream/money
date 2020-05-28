cafaa8a7613c556ff3d45dba1f5668c98c4d1222
li <1505899522@qq.com>
2020-05-28 18:06:46 +0800

    创建Layout组件并引入icons

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">Money.vue</div>
-    <Nav />
-  </div>
+  <Layout>
+    <p>Money.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -12,15 +11,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  border: 1px solid blue;
-  overflow: auto;
-  flex-grow: 1;
-}
 </style>
