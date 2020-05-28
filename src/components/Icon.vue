91858a65e56e76de44ec2b84ab722d7e0ddb3c4c
li <1505899522@qq.com>
2020-05-28 23:16:15 +0800

    解决eslint报错问题

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
   requireContext.keys().forEach(requireContext);
 try {
   importAll(require.context("../assets/icons", true, /\.svg$/));
