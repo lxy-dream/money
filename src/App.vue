84312333ede751fb9e1b4ea203e2d78ba5f4b8c7
li <1505899522@qq.com>
2020-05-29 14:24:36 +0800

    写好了HTML和CSS

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
-  font-family: $font-hei;
+  font-family: $font-kai;
+  font-size: 16px;
   line-height: 1.5;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -23,8 +23,8 @@ export default {
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 nav {
+  @extend %outerShadow;
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
   font-size: 12px;
   > .item {
@@ -34,7 +34,7 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    background: #ffcc00;
+    background: #ffda44;
     .icon {
       width: 32px;
       height: 32px;
