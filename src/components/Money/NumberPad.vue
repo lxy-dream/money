d3f3f13136288710c1cd38bb89b3bcbb121e2b1c
li <1505899522@qq.com>
2020-05-31 16:58:02 +0800

    创建标签页面

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -62,6 +62,7 @@ export default class NumberPad extends Vue {
     this.$emit("update:value", this.output);
     this.$emit("submit", this.output);
     this.output = "0";
+    window.alert("添加成功");
   }
 }
 </script>
