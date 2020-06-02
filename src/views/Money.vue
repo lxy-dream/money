f732f281d1436621cbdf1fe5fe0e116d0eb19597
li <1505899522@qq.com>
2020-06-02 18:39:54 +0800

    解决 Tag.vue 的 bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,7 +5,7 @@
     <div class="notes">
       <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes" />
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
+    <Tags />
   </Layout>
 </template>
 
@@ -22,7 +22,6 @@ import store from "@/store/index2.ts";
   components: { Tags, FormItem, Types, NumberPad }
 })
 export default class Money extends Vue {
-  tags = store.tagList;
   recordList = store.recordList;
   record: RecordItem = {
     tags: [],
@@ -30,9 +29,7 @@ export default class Money extends Vue {
     type: "-",
     amount: 0
   };
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
-  }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
