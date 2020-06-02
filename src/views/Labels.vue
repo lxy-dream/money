f6530fd02ea174342eb6bf632b2899e44d55288e
li <1505899522@qq.com>
2020-06-02 16:34:34 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -22,16 +22,12 @@ import Button from "@/components/Button.vue";
   components: { Button }
 })
 export default class Labels extends Vue {
-  tags = window.tagList;
+  tags = window.tagList; //读的时候是去window上面读的
+
   createTag() {
     const name = window.prompt("请输出标签名");
     if (name) {
-      const message = tagListModel.create(name);
-      if (message === "duplicated") {
-        window.alert("标签名重复了");
-      } else if (message === "success") {
-        window.alert("添加成功");
-      }
+      window.createTag(name);
     }
   }
 }
