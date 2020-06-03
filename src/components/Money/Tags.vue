dc5044de1d9e4194be6e90f2cc19938b0485936b
li <1505899522@qq.com>
2020-06-03 12:32:19 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -21,14 +21,17 @@ import { Component, Prop } from "vue-property-decorator";
 @Component({
   computed: {
     tagList() {
-      // TODO
-      // return this.$store.fetchTags();
-      return [];
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit("fetchTags");
+  }
+
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -43,6 +46,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert("标签名不能为空");
     }
+    this.$store.commit("createTag", name);
   }
 }
 </script>
